0e58fdbd2fdb4187a50317643d1368bf7b101372
Steven Doran <78985334+S81D@users.noreply.github.com>
2025-04-21 08:57:56 -0500

    Update run_container_job.sh

--- submit_simple_toolchain/run_container_job.sh
+++ submit_simple_toolchain/run_container_job.sh
@@ -1,31 +1,46 @@
 #!/bin/bash 
-# James Minock 
+# Steven Doran
 
-# logfile
-touch /srv/logfile_2.txt 
+# job script that runs within the container and executes your toolchain
 
-# place the input file containing the necessary data files in the toolchain
-echo "my_inputs.txt paths:" >> /srv/logfile_2.txt
-cat /srv/my_inputs.txt >> /srv/logfile_2.txt 
-\cp /srv/my_inputs.txt /srv/MyToolAnalysis_2_26_23/configfiles/BeamClusterAnalysis/ 
-echo "" >> /srv/logfile_2.txt
+ARG1=$1     # example arguments passed down
+ARG2=$2     # use these in the script as needed
+
+# ------------------------------------------
+# logfile for toolchain verbose
+touch /srv/logfile_${ARG1}_${ARG2}.txt 
+
+# for toolchains that need a list file: place the input file containing the necessary data files in the toolchain
+rm /srv/DQ_tool//configfiles/PrintDQ/my_inputs.txt      # in case there is conflict with overwite
+\cp /srv/my_inputs.txt /srv/ToolAnalysis/configfiles/PrintDQ/ 
+
+echo "my_inputs.txt paths:" >> /srv/logfile_${ARG1}_${ARG2}.txt    # verify files are as expected
+cat /srv/my_inputs.txt >> /srv/logfile_${ARG1}_${ARG2}.txt  
+echo "" >> /srv/logfile_${ARG1}_${ARG2}.txt 
+# ------------------------------------------
+
 
 # enter ToolAnalysis directory 
-cd MyToolAnalysis_2_26_23/ 
+cd ToolAnalysis/ 
 
 # set up paths and libraries 
 source Setup.sh 
 
-# Run the toolchain, and output verbose to log file 
-./Analyse configfiles/BeamClusterAnalysis/ToolChainConfig >> /srv/logfile_beamcluster.txt 
+echo "Running Toolchain..." >> /srv/logfile_${ARG1}_${ARG2}.txt 
+echo "" >> /srv/logfile_${ARG1}_${ARG2}.txt 
 
-# log files
-echo "ToolAnalysis directory contents:" >> /srv/logfile_2.txt
-ls -lrth >> /srv/logfile_2.txt
-echo "" >> /srv/logfile_2.txt
-echo "ToolChain files:" >> /srv/logfile_2.txt
-ls configfiles/BeamClusterAnalysis/ >> /srv/logfile_2.txt 
-echo "" >> /srv/logfile_2.txt
+# Run the toolchain, and output verbose to log file 
+./Analyse configfiles/PrintDQ/ToolChainConfig >> /srv/logfile_${ARG1}_${ARG2}.txt  
+
+
+# ------------------------------------------
+# check what files are created (for debugging)
+echo "" >> /srv/logfile_${ARG1}_${ARG2}.txt 
+echo "ToolAnalysis directory contents:" >> /srv/logfile_${ARG1}_${ARG2}.txt 
+ls -lrth >> /srv/logfile_${ARG1}_${ARG2}.txt 
+echo "" >> /srv/logfile_${ARG1}_${ARG2}.txt 
+# ------------------------------------------
+
 
 # copy any produced files to /srv for extraction
 cp *.ntuple.root /srv/ 
